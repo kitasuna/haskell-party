5ededac6b649864aa91a3da2b188f2b19c57fef3
Eric Jelliffe <eric.jelliffe@fastretailing.com>
2018-06-26 10:49:12 +0900

    Add comments to product code

--- product.hs
+++ product.hs
@@ -1,41 +1,49 @@
-type Name = String
-type Inventory = Int
-type Colors = [Color]
-
+-- `data` keyword is used for defining new types
+-- Below, we'll define Color as a "sum type", which
+-- is sort of like an enum in other languages.
 data Color =
   Blue | Red | Purple | Black | White
   deriving (Eq, Show)
 
-data Product =
-  Product Name Colors Inventory
+-- `type` keyword is for type aliasing
+-- This is useful for improving readability of type signatures.
+-- Note that we can also alias custom types, as we did when we
+-- created an alias for an array of our Color type below.
+type Name = String
+type Inventory = Int
+type Colors = [Color]
+
+-- Here we'll make a FRProduct type as a combination
+-- of a Name (string), an array of Colors, and an
+-- Inventory (int)
+data FRProduct =
+  FRProduct Name Colors Inventory
   deriving (Eq, Show)
 
-getName :: Product -> Name
-getName (Product name _ _) = name
+-- This is a simple pattern matching example
+-- We can extract the name from an FRProduct
+-- by pattern matching on the various parts of the
+-- FRProduct constructor.
+-- Using the underscore in pattern matching allows us to
+-- ignore arguments that we don't need
+getName :: FRProduct -> Name
+getName (FRProduct name _ _) = name
 
-inStock :: Product -> Bool
-inStock (Product _ _ 0) = False
+-- Check inventory using pattern matching
+-- Negative numbers would return an incorrect result
+inStock :: FRProduct -> Bool
+inStock (FRProduct _ _ 0) = False
 inStock _ = True
 
-inStockG :: Product -> Bool
-inStockG (Product _ _ inv)
+-- Check inventory using guards
+-- Here, we properly handle negative numbers
+inStockG :: FRProduct -> Bool
+inStockG (FRProduct _ _ inv)
   | inv > 0 = True
   | otherwise = False
 
-
-
-
-
-
-
-
-
-
-isAvailableG :: Product -> Bool
-isAvailableG (Product n c i)
-  | i > 0 = True
-  | otherwise = False
-
-ironman = Product "Ironman shirt" [Blue, Purple, Black] 42
-spiderman = Product "Spiderman shirt" [White, Red] 0
-magneto = Product "Magneto shirt" [White, Red] (-2)
+-- Define a few test products
+-- so we can mess around in the REPL
+ironman = FRProduct "Ironman shirt" [Blue, Purple, Black] 42
+spiderman = FRProduct "Spiderman shirt" [White, Red] 0
+magneto = FRProduct "Magneto shirt" [White, Red] (-2)
